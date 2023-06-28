be013206f6dbde18ef41058c846d930b0c8a9d7c
Jordan Frankfurt <jordan@CORN-Jordan-949.frankfurt>
2023-06-28 16:21:59 -0400

    fix lint issues

--- src/app/scan/qr_code_reader.tsx
+++ src/app/scan/qr_code_reader.tsx
@@ -6,6 +6,7 @@ interface QrCodeProps {
   onData: (data: string) => void;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function QrCodeReader({ onData }: QrCodeProps) {
   const video = useRef<HTMLVideoElement>(null);
 
