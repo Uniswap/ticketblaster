09f499578a2804854290c0450efd207f839c1205
Jordan Frankfurt <jordan@CORN-Jordan-949.frankfurt>
2023-06-28 16:54:41 -0400

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         asdf
-    </div>
+      </div>
     </main>
   )
 }
