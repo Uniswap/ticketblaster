96831a649fe317fa7148f431524679b8e41f7275
Zach Pomerantz <zzmp@uniswap.org>
2023-06-28 16:02:58 -0400

    feat: scaffold qr_code_reader

---
+++ src/app/scan/qr_code_reader.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useRef } from "react"
+
+interface QrCodeProps {
+  onData: (data: string) => void
+}
+
+export default function QrCodeReader({ onData }: QrCodeProps) {
+  const video = useRef<HTMLVideoElement>(null)
+
+  return (
+    <video className="w-full h-full" ref={video} />
+  )
+}
