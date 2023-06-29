5d10e0cf6e7bdb43fe302d10f26b3f4e1e594c30
Zach Pomerantz <zzmp@uniswap.org>
2023-06-29 01:22:11 -0400

    refactor: status enums

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -1,49 +1,55 @@
 'use client'
 
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import QrCodeReader from './qr_code_reader'
 import styles from './scan.module.scss'
 
-export default function Scan() {
-  const [data, setData] = useState<boolean | null>(null)
-  const onQRCodeParsed = (data: string) =>
-    fetch('/api/validate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ data }),
-    })
-      .then((res) => setData(res.ok))
-      .catch(() => setData(false))
+const enum Status {
+  Unknown = 'unknown',
+  Valid = 'valid',
+  Invalid = 'invalid',
+  Error = 'error',
+}
 
-  const clearData = useCallback(() => setData(null), [])
+export default function Scan() {
+  const [data, setData] = useState<object>()
+  const [status, setStatus] = useState<Status>(Status.Unknown)
+  const resetStatus = useCallback(() => setStatus(Status.Unknown), [])
 
   useEffect(() => {
     if (data) {
-      setTimeout(clearData, 5 * 1000)
+      fetch('/api/validate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ data }),
+      })
+        .then((res) => setStatus(res.ok ? Status.Valid : Status.Invalid))
+        .catch(() => setStatus(Status.Error))
     }
   }, [data])
-
-  const scanHoleClasses = useMemo(() => {
-    const classes = [styles.scannerHole, styles.targetBorder]
-    if (data) {
-      scanHoleClasses.push(styles.success)
-    }
-    return classes
-  }, [])
 
   return (
     <main className={styles.wrapper}>
       <h1 className={styles.title}>Scan Ticket</h1>
-      <QrCodeReader onData={onQRCodeParsed} onError={console.warn} />
       <div className={styles.scannerOverlay}>
-        <div className={scanHoleClasses.join(' ')} />
+        <QrCodeReader onData={setData} onError={console.warn} />
+        <div
+          className={[
+            styles.scannerHole,
+            styles.targetBorder,
+            status === Status.Valid ? styles.success : '',
+          ].join(' ')}
+        />
       </div>
       <div className={styles.metadata}>
-        {data === null && <p>Scan a ticket to validate it</p>}
-        {data === true && <p onClick={clearData}>Valid ticket</p>}
-        {data === false && <p onClick={clearData}>Invalid ticket</p>}
+        {status === Status.Unknown && <p>Scan a ticket to validate it</p>}
+        {status === Status.Valid && <p onClick={resetStatus}>Valid ticket</p>}
+        {status === Status.Invalid && (
+          <p onClick={resetStatus}>Invalid ticket</p>
+        )}
+        {status === Status.Error && <p onClick={resetStatus}>Error</p>}
       </div>
     </main>
   )
