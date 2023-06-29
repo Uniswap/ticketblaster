eedd6b9818a33c6b27c2d21037fdaa941cdc9173
Jordan Frankfurt <jordan@uniswap.org>
2023-06-29 13:29:52 -0400

    test camera switching

--- src/app/scan/qr_code_reader.tsx
+++ src/app/scan/qr_code_reader.tsx
@@ -3,6 +3,8 @@ import BarcodeDetector from 'barcode-detector'
 import assert from 'assert'
 import useAnimationFrame from '@/hooks/useAnimationFrame'
 import styles from './scan.module.scss'
+import { RefreshCw } from 'react-feather'
+import useSupportsEnvironmentCamera from './useSupportsEnvironmentCamera'
 
 const SCAN_LINE_SIZE = 512
 
@@ -44,9 +46,11 @@ export default function QrCodeReader({ onData, onError }: QrCodeProps) {
       willReadFrequently: true,
     })
     assert(context, 'canvas context is null')
-    const dimensions = (video.current.srcObject as MediaStream | null)
-      ?.getVideoTracks()[0]
-      .getSettings()
+    const srcObject = video.current.srcObject as MediaStream | null
+    if (!srcObject) return
+
+    const dimensions = srcObject?.getVideoTracks()[0].getSettings()
+
     if (!dimensions) return
     const { width, height } = dimensions
     assert(width && height, 'video has no dimensions')
@@ -71,6 +75,19 @@ export default function QrCodeReader({ onData, onError }: QrCodeProps) {
     }
   }, [detector, onData, onError])
 
+  const supportsEnvironment = useSupportsEnvironmentCamera()
+
+  const toggleUserFacingOrEnvironmentCamera = useCallback(() => {
+    assert(video.current, 'video ref is null')
+    const stream = video.current.srcObject as MediaStream | null
+    if (!stream || !supportsEnvironment) return
+    const track = stream.getVideoTracks()[0]
+    const facingMode = track.getSettings().facingMode
+    track.applyConstraints({
+      facingMode: facingMode === 'user' ? 'environment' : 'user',
+    })
+  }, [])
+
   useEffect(requestMedia, [requestMedia])
   useAnimationFrame(detect)
 
@@ -83,6 +100,12 @@ export default function QrCodeReader({ onData, onError }: QrCodeProps) {
         height={SCAN_LINE_SIZE}
       />
       <video playsInline autoPlay ref={video} className={styles.video} />
+      {supportsEnvironment && (
+        <RefreshCw
+          className={styles.switchView}
+          onClick={toggleUserFacingOrEnvironmentCamera}
+        />
+      )}
     </>
   )
 }
