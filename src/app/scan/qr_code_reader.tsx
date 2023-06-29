87f1c2f90f2c74cc0fbdd264976c4d8ae110fc4a
Zach Pomerantz <zzmp@uniswap.org>
2023-06-29 12:12:07 -0400

    chore: rm unused useState

--- src/app/scan/qr_code_reader.tsx
+++ src/app/scan/qr_code_reader.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState, useRef } from 'react'
+import { useCallback, useEffect, useMemo, useRef } from 'react'
 import BarcodeDetector from 'barcode-detector'
 import assert from 'assert'
 import useAnimationFrame from '@/hooks/useAnimationFrame'
