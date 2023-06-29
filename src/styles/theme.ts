2fafee0b4b96d4f0f972118c30f74685e003a4c0
Jordan Frankfurt <jordan@CORN-Jordan-949.frankfurt>
2023-06-28 21:50:13 -0400

    feat: mint button

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  black: '#000',
+  blue: '#a2e2ff',
+  orange: '#ffcc99',
+  green: '#a4ffca',
+  pink: '#ffc9df',
+  purple: '#dab5ff',
+  yellow: '#fff6ae',
+  white: '#ffffff',
+  hotPink: '#fa0f95',
+  rainbow:
+    'radial-gradient(142.75% 65.98% at 50% 50%, #ffb9b9 0%, #fff7ad 26.04%, #97fff3 54.17%, #cfbcff 77.08%, #ffb9b9 100%)',
+
+  pinkShadow: '0px 0px 30px 5px hsla(326 96% 52% / 0.2)',
+  none: 'none',
+}
+
+export default theme
