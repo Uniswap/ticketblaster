b8ba84fc3bde3e4b560d31cd8de85a9c20452c93
Jordan Frankfurt <jordan@CORN-Jordan-949.frankfurt>
2023-06-28 22:15:25 -0400

    link to zora for mint

--- src/app/mint/page.tsx
+++
@@ -1,9 +0,0 @@
-import Button from '@/components/Button'
-
-export default function Mint() {
-  return (
-    <main>
-      <Button>mint</Button>
-    </main>
-  )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import styles from '../styles/Index.module.scss'
 export default function Home() {
   return (
     <main className={styles.wrapper}>
-      <Link className={styles.link} href="/mint">
+      <Link
+        className={styles.link}
+        target="_blank"
+        referrerPolicy="no-referrer"
+        href="https://zora.co/collect/eth:0x0366c4fe5b9475afcb8ce7d94aac3668b6db8247/1"
+      >
         Mint
       </Link>
       <Link className={styles.link} href="/scan">
